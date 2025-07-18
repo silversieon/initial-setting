d4d6699ff0769555064c069b76aebf06a08eb4ea
Sieon Keum <keumsiun0503@gmail.com>
2025-07-18 16:28:07 +0900

    :sparkles: Feat: deploy set

--- src/main/java/com/practice/initialsetting/InitialSettingApplication.java
+++ src/main/java/com/practice/initialsetting/InitialSettingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InitialSettingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(InitialSettingApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(InitialSettingApplication.class, args);
+  }
 }
